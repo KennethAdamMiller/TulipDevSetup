89c2bb2afff02bea366c8a1e6f798b450d3fce63
Kenneth Adam Miller <kennethadammiller@gmail.com>
2014-04-15 03:30:43 -0700

    add setup script, should be complete, hasn't been tested

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt-get install clang g++ make vim cmake-gui python-sip python-sip-dev libpng12-dev libjpeg-dev zlib1g-dev zlib-bin zlibc libfreetype6 libfreetype6-dev libglew1.8 libxml2-dev terminator qt5-default libqt5xmlpatterns5 libqt5xmlpatterns5-dev libqt5webkit5 libqt5webkit5-dev libqt5widgets5 libqt5xml5 libqt5opengl5 libqt5network5 libqt5gui5 qtquick1-5-dev qttools5-dev-tools qt5declarative5-dev libqt5webkit5-qmlwebkitplugin qtlocation5-dev libqt5location5 libqt5sensors5 qtsensors5-dev libpython-dev python-dev python-sphinx doxygen binutils-dev
+
+mkdir -p ~/workspace/tulip/build/x64/debug/Qt5
+cd ~/workspace/tulip
+svn checkout svn://svn.code.sf.net/p/auber/code/tulip ./tulip-src
+cd tulip-src
+
+Qt5Prefix="/usr/lib/x86_64-linux-gnu/cmake/Qt5"
+cmake ~/workspace/tulip/build/x64/debug/Qt5/ ~/workspace/tulip/tulip-src/tulip -G "Unix Makefiles" \
+	-D"USE_QT5_IF_INSTALLED=true" \
+	-D"CMAKE_CXX_FLAGS=-std=c++11" \
+	-D"CMAKE_INSTALL_PREFIX=~/workspace/tulip/build/x64/debug/Qt5/install" \
+	-D"CMAKE_BUILD_TYPE=Debug"
+
+if [[ -z $PYTHONPATH ]]; then
+	echo 'export PYTHONPATH="~/workspace/tulip/build/x64/debug/Qt5/install/lib/python"' >> ~/.bashrc
+fi
+if [[ -z $LD_LIBRARY_PATH ]]; then
+	echo 'export LD_LIBRARY_PATH="~/workspace/tulip/build/x64/debug/Qt5/install/lib"' >> ~/.bashrc
+fi
